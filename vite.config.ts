d51525483f570d94a28313bfbf2cd2a92f19882f
Traff444 <124590449+Traff444@users.noreply.github.com>
2026-01-13 22:06:18 +0300

    Настройка деплоя на GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,10 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/agrosnab/',
   plugins: [react()],
   server: {
-    port: 8011,
+    port: 8300,
   },
   optimizeDeps: {
     exclude: ['lucide-react'],
